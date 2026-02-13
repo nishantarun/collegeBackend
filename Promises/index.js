99bad63e6d41fef08053ea568ad277d3c88ac94a
Nishant Arun <nishantarun2512@gmail.com>
2026-02-13 15:01:23 +0530

    Chaining with scope

--- Promises/index.js
+++ Promises/index.js
@@ -15,3 +15,37 @@ fetchData.catch((error) => {
 const login = () => {
   return Promise.resolve("Login success");
 };
+
+const getUser = () => {
+  return Promise.resolve("User Found");
+};
+
+const getUserName = () => {
+  return Promise.resolve("User Name Found");
+};
+
+// CALLBACK HELL
+// login().then(() => {
+//   getUser().then(() => {
+//     getUserName().then(() => {
+//       console.log("callback hell");
+//     });
+//   });
+// });
+
+// CHAINING instead CALLBACK
+
+login()
+  .then(getUser)
+  .then(getUserName)
+  .then(console.log("Chaining"))
+  .catch((err) => console.log(err));
+
+Promise.resolve(10)
+  .then((x) => {
+    return x + 10;
+  })
+  .then((x) => {
+    return x + 20;
+  })
+  .then(console.log);
