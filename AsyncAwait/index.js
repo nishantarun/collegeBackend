bbd1f242fe4be8945afadfb2f0646f0359ec612e
Nishant Arun <nishantarun2512@gmail.com>
2026-02-16 11:45:31 +0530

    second commit

--- AsyncAwait/index.js
+++ AsyncAwait/index.js
@@ -1,12 +1,29 @@
-const sample = async () => {
-  console.log("Started");
-  await fetch("http://jsonplaceholder.typicode.com/posts/1")
+// const sample = async () => {
+//   console.log("Started");
+//   await fetch("http://jsonplaceholder.typicode.com/posts/1")
+//     .then((res) => res.json())
+//     .then((json) => console.log(json));
+//   console.log("Ended");
+// };
+
+// console.log("Process 1-------------------");
+// sample();
+// console.log("Process 2-------------------");
+
+const sample1 = async () => {
+  const func1 = fetch("http://jsonplaceholder.typicode.com/posts/1")
     .then((res) => res.json())
     .then((json) => console.log(json));
-  console.log("Ended");
+
+  const func2 = fetch("http://jsonplaceholder.typicode.com/posts/2")
+    .then((res) => res.json())
+    .then((json) => console.log(json));
+
+  const a = await func1;
+  const b = await func2;
+
+  console.log("🚀 sample ~ b");
+  console.log("🚀 sample ~ a");
 };
 
-
-console.log("Process 1-------------------");
-sample();
-console.log("Process 2-------------------");
+sample1();
